41bb93c4b8fa9f34e3bd870c3881a0c7c7d94a68
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2023-03-07 10:08:47 +0100

    Added rol enum to user attributes.

--- userManagement/src/main/java/com/uma/southdevelopers/User.java
+++ userManagement/src/main/java/com/uma/southdevelopers/User.java
@@ -3,16 +3,21 @@ package com.uma.southdevelopers;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 public class User {
+
+    enum Rol {
+        ESTUDIANTE, RESPONSABLE_SEDE, VIGILANTE_AULA, CORRECTOR, ADMINISTRADOR
+    }
+
     @Id @GeneratedValue
     private Long userId;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
 
 }
