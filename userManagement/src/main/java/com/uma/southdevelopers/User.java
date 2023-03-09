2deddbe13c083c207ebeedfa59d800d7dbee639f
joseantoniols1212 <joseantonioluquesalguero1212@gmail.com>
2023-03-09 15:58:43 +0100

    Added name and surname to user

--- userManagement/src/main/java/com/uma/southdevelopers/User.java
+++ userManagement/src/main/java/com/uma/southdevelopers/User.java
@@ -16,6 +16,8 @@ public class User {
     @Id @GeneratedValue
     private Long userId;
     private String email;
+    private String name;
+    private String surname;
     private String password;
     @Enumerated(EnumType.STRING)
     private Rol rol;
