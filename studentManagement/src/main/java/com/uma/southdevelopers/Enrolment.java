8df94ab81c7df388ea172b192d2a1bf71b76b77c
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-03-13 12:28:10 +0100

    added enrolment primary key

--- studentManagement/src/main/java/com/uma/southdevelopers/Enrolment.java
+++ studentManagement/src/main/java/com/uma/southdevelopers/Enrolment.java
@@ -8,8 +8,12 @@ import java.util.Date;
 @Entity
 @Data
 public class Enrolment {
+
+
+    @Id
     @Temporal(TemporalType.DATE)
     private Date date;
     @OneToOne(fetch = FetchType.LAZY)
     private Student student;
+
 }
