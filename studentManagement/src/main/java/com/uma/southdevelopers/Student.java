a053df57556f3d1ba2dd8dc067a83877a24ead7e
Your Name <you@example.com>
2023-04-15 11:06:10 +0200

    Added @NonNull to many attributes

--- studentManagement/src/main/java/com/uma/southdevelopers/Student.java
+++ studentManagement/src/main/java/com/uma/southdevelopers/Student.java
@@ -3,6 +3,7 @@ package com.uma.southdevelopers;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.*;
 import java.util.List;
@@ -15,14 +16,22 @@ public class Student {
 
     @Id
     private String DNI;
+
+    @NonNull
     private String name;
+
     private String surnames;
+    
     @OneToMany
     private List<Enrolment> enrolment;
+
     private String email;
+
     private String phoneNumber;
+
     @ManyToMany
     private List<SpecialNeeds> specialNeeds;
+
     private String examsId;
     private String school;
 
