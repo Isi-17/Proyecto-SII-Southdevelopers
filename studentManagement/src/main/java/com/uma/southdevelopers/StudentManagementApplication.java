503a409afca435170fae00a6737bf867c23398d0
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-04-16 19:44:22 +0200

    Changed StudentManagementApplication to SpringApplication

--- studentManagement/src/main/java/com/uma/southdevelopers/StudentManagementApplication.java
+++ studentManagement/src/main/java/com/uma/southdevelopers/StudentManagementApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementApplication.class, args);
+        // prueba
     }
 }
