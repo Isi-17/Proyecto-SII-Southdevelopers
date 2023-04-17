c0bddd5bfe3a879cb86d18ce91854ad51c2ca3f8
Jacobo-EG <0619975360@uma.es>
2023-04-17 21:28:51 +0200

    Added the aplication.properties file and other minor changes

--- userManagement/src/main/java/com/uma/southdevelopers/repositories/UserRepository.java
+++ userManagement/src/main/java/com/uma/southdevelopers/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 
 import com.uma.southdevelopers.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User save(User user);
