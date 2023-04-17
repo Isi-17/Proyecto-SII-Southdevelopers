3b595e8a06206da99f6432520843394e2153082d
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-04-17 12:50:44 +0200

    Added Institute entity

---
+++ studentManagement/src/main/java/com/uma/southdevelopers/entities/Institute.java
@@ -0,0 +1,24 @@
+package com.uma.southdevelopers.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class Institute {
+    @Id
+    private Long id;
+    private String name;
+    private String address1;
+    private String address2;
+    private String city;
+    private Integer zipcode;
+    private String country;
+}
