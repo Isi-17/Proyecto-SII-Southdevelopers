777eef36c80175582f1878235caf23dd5682ea5b
xexu65 <xexu65@uma.es>
2023-04-17 18:59:15 +0200

    added class Tests.java

---
+++ studentManagement/src/main/java/com/uma/southdevelopers/tests/Tests.java
@@ -0,0 +1,119 @@
+package com.uma.southdevelopers.tests;
+
+import org.junit.jupiter.api.Test;
+import com.uma.southdevelopers.entities.Enrolment;
+import com.uma.southdevelopers.entities.Institute;
+import com.uma.southdevelopers.entities.Student;
+import com.uma.southdevelopers.repositories.EnrolmentRepository;
+import com.uma.southdevelopers.repositories.InstituteRepository;
+import com.uma.southdevelopers.repositories.StudentRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de estudiantes")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class Tests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private StudentRepository studentRepo;
+
+    @Autowired
+    private InstituteRepository instituteRepo;
+
+    @Autowired
+    private EnrolmentRepository enrolmentRepo;
+
+    private URI uri(String scheme, String host, int port, String ...paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+                .scheme(scheme)
+                .host(host).port(port);
+        for(String path: paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.get(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        return peticion;
+    }
+
+    private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.delete(uri)
+                .build();
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(object);
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.put(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(object);
+        return peticion;
+    }
+
+    private void checkFields(Student expected, Student actual) {
+        assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
+        assertThat(actual.getDni()).isEqualTo(expected.getDni());
+        assertThat(actual.getEmail()).isEqualTo(expected.getEmail());
+        assertThat(actual.getIdSede()).isEqualTo(expected.getIdSede());
+        assertThat(actual.getTelefono()).isEqualTo(expected.getTelefono());
+        assertThat(actual.getInstituto()).isEqualTo(expected.getInstituto());
+        assertThat(actual.getMateriasMatriculadas()).isEqualTo(expected.getMateriasMatriculadas());
+    }
+
+    private void checkFields(Institute expected, Institute actual) {
+        assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
+        assertThat(actual.getPais()).isEqualTo(expected.getPais());
+        assertThat(actual.getLocalidad()).isEqualTo(expected.getLocalidad());
+        assertThat(actual.getCodigoPostal()).isEqualTo(expected.getCodigoPostal());
+        assertThat(actual.getDireccion1()).isEqualTo(expected.getDireccion1());
+        assertThat(actual.getDireccion2()).isEqualTo(expected.getDireccion2());
+    }
+
+    private void checkFields(Enrolment expected, Enrolment actual) {
+        assertThat(actual.getEliminada()).isEqualTo(expected.getEliminada());
+        assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
+        assertThat(actual.getEstudiante()).isEqualTo(expected.getEstudiante());
+    }
+
+
+
+
+
+
+
+}
