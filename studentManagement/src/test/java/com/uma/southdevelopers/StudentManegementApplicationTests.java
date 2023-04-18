87939df8a8760eb37761d637b5aea4e6005db101
xexu65 <xexu65@uma.es>
2023-04-18 16:17:52 +0200

    changed StudentManegementApplicationTests.java

--- studentManagement/src/test/java/com/uma/southdevelopers/StudentManegementApplicationTests.java
+++ studentManagement/src/test/java/com/uma/southdevelopers/StudentManegementApplicationTests.java
@@ -1,6 +1,11 @@
-package com.uma.southdevelopers.tests;
+package com.uma.southdevelopers;
 
-import org.junit.jupiter.api.Test;
+import com.uma.southdevelopers.dtos.InstituteDTO;
+import com.uma.southdevelopers.dtos.StudentDTO;
+import com.uma.southdevelopers.entities.CompleteName;
+import org.junit.jupiter.api.Nested;
+import org.springframework.core.ParameterizedTypeReference;
+import org.testng.annotations.Test;
 import com.uma.southdevelopers.entities.Enrolment;
 import com.uma.southdevelopers.entities.Institute;
 import com.uma.southdevelopers.entities.Student;
@@ -20,13 +25,14 @@ import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriBuilderFactory;
 
 import java.net.URI;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayName("En el servicio de estudiantes")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-public class Tests {
+public class StudentManegementApplicationTests {
 
     @Autowired
     private TestRestTemplate restTemplate;
@@ -39,9 +45,6 @@ public class Tests {
 
     @Autowired
     private InstituteRepository instituteRepo;
-
-    @Autowired
-    private EnrolmentRepository enrolmentRepo;
 
     private URI uri(String scheme, String host, int port, String ...paths) {
         UriBuilderFactory ubf = new DefaultUriBuilderFactory();
@@ -104,16 +107,88 @@ public class Tests {
         assertThat(actual.getDireccion2()).isEqualTo(expected.getDireccion2());
     }
 
-    private void checkFields(Enrolment expected, Enrolment actual) {
-        assertThat(actual.getEliminada()).isEqualTo(expected.getEliminada());
-        assertThat(actual.getNombre()).isEqualTo(expected.getNombre());
-        assertThat(actual.getEstudiante()).isEqualTo(expected.getEstudiante());
+    @Nested
+    @DisplayName("cuando la base de datos está vacía")
+    public class EmptyDataBase {
+
+        @Test
+        @DisplayName("devuelve error al acceder a un estudiante concreto")
+        public void errorConEstudianteConcreto() {
+            var peticion = get("http", "localhost", port, "/localhost/1");
+
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<StudentDTO>() {});
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+
+        @Test
+        @DisplayName("devuelve error al acceder a un instituto concreto")
+        public void errorConInstitutoConcreto() {
+            var peticion = get("http", "localhost", port, "/localhost/1");
+
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<InstituteDTO>() {});
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(404);
+        }
+
+        @Test
+        @DisplayName("devuelve una lista vacía de estudiantes")
+        public void devuelveListaVaciaEstudiantes(){
+            var peticion = get("http", "localhost", port, "/localhost");
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<List<StudentDTO>>() {});
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("devuelve una lista vacía de institutos")
+        public void devuelveListaVaciaInstitutos(){
+            var peticion = get("http", "localhost", port, "/localhost");
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<List<InstituteDTO>>() {});
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+
+        @Test
+        @DisplayName("inserta correctamente un estudiante")
+        public void insertaEstudiante(){
+
+            CompleteName cn = new CompleteName();
+            cn.setNombre("Jesus");
+            cn.setApellido1("Escudero");
+            cn.setApellido2("Moreno");
+
+            // Preparamos el ingrediente a insertar
+            var estudiante = StudentDTO.builder()
+                    .nombre(cn)
+                    .build();
+
+            // Preparamos la peticion con el estudiante dentro
+            var peticion = post("http", "localhost", port, "/localhost", estudiante);
+
+            // Invocamos al servicio REST
+            var respuesta = restTemplate.exchange(peticion, Void.class);
+
+            // Comprobamos el resultado
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(201);
+            assertThat(respuesta.getHeaders().get("Location").get(0))
+                    .startsWith("http://localhost:" + port + "/Localhost");
+
+            List<Student> estudiantesBD = studentRepo.findAll();
+            assertThat(estudiantesBD).hasSize(1);
+
+
+
+        }
+
+
+
+
+
+
     }
-
-
-
-
-
-
-
 }
