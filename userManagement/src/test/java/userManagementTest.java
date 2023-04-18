ac8826d6ba51e0687ce5b9eefdbf0863e54dd7c3
Jacobo-EG <0619975360@uma.es>
2023-04-18 19:51:29 +0200

    Began the tests for userManagement

--- userManagement/src/test/java/userManagementTest.java
+++ userManagement/src/test/java/userManagementTest.java
@@ -4,6 +4,9 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.uma.southdevelopers.entities.User;
+import com.uma.southdevelopers.repositories.UserRepository;
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -24,4 +27,70 @@ import org.springframework.web.util.UriBuilderFactory;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class userManagementTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value(value="${local.server.port}")
+    private int port;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    private URI uri(String scheme, String host, int port, String ...paths) {
+        UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+                .scheme(scheme)
+                .host(host).port(port);
+        for (String path: paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+    }
+
+    private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.get(uri)
+                .accept(MediaType.APPLICATION_JSON)
+                .build();
+        return peticion;
+    }
+
+    private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.delete(uri)
+                .build();
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(object);
+        return peticion;
+    }
+
+    private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host,port, path);
+        var peticion = RequestEntity.put(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(object);
+        return peticion;
+    }
+    @Nested
+    @DisplayName("Con la Base de Datos Vacia")
+    public class BaseDeDatosVacia {
+        @Test
+        @DisplayName("devuelve una lista vacía de usuarios")
+        public void devuelveListaVaciaProductos() {
+            var peticion = get("http", "localhost",port, "");
+
+            var respuesta = restTemplate.exchange(peticion,
+                    new ParameterizedTypeReference<List<User>>() {});
+
+            assertThat(respuesta.getStatusCode().value()).isEqualTo(200);
+            assertThat(respuesta.getBody()).isEmpty();
+        }
+    }
 }
