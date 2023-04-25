603ab625eff82c7d7c91484a28e2a453c51eb381
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-04-25 19:55:06 +0200

    Started Angular Application, got get petition working

--- studentAngular/src/app/app.component.ts
+++ studentAngular/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Instituto } from './models/instituto.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'studentAngular';
+  http = inject(HttpClient);
+  institutos: Instituto[] = [];
+
+  ngOnInit() {
+    this.http.get<Instituto[]>('http://localhost:8080/institutos')
+      .subscribe((data) => {
+        this.institutos = data;
+      });
+  }
+
+  deleteInstituto(id: number) {
+    this.http.delete('http://localhost:8080/institutos/' + id);
+  }
+
+  
+
 }
--- studentAngular/src/app/app.module.ts
+++ studentAngular/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule }  from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InstitutoComponent } from './instituto/instituto.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InstitutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
