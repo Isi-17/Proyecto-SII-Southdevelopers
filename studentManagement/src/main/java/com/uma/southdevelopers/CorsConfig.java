b1dfa09ee15d5d83bf0f1cfd80838b0aa15585fa
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-04-25 23:46:09 +0200

    Fixed cors configuration

--- studentManagement/src/main/java/com/uma/southdevelopers/CorsConfig.java
+++ studentManagement/src/main/java/com/uma/southdevelopers/CorsConfig.java
@@ -1,2 +1,23 @@
-package com.uma.southdevelopers;public class CorsConfig {
+package com.uma.southdevelopers;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Configura el mapeo para todas las rutas
+                        .allowedOrigins("http://localhost:4200") // Permite solicitudes desde esta URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite los métodos HTTP permitidos
+                        .allowCredentials(true); // Permite el envío de cookies
+            }
+        };
+    }
 }
