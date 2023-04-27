9404cce4c22499b6363a8af493e136d94d154ef4
Jacobo-EG <0619975360@uma.es>
2023-04-27 16:58:32 +0200

    Added the control via cookie with jwt

--- frontend/userAngular/src/app/login/login.component.ts
+++ frontend/userAngular/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { usuario } from '../models/usuario.models'
 import { usersService } from '../usuarios/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,9 +12,16 @@ export class LoginComponent {
   usuario: usuario = {email: '',
   password: ''};
 
-  constructor(public userService: usersService) {}
+  constructor(public userService: usersService, public router: Router) {} //
 
   login() {
-    this.userService.login(this.usuario).subscribe((data) => { console.log(data); });
+    this.userService.login(this.usuario).subscribe(
+      data => {
+        this.userService.setToken(data.token);
+        this.router.navigateByUrl("/");
+      },
+      error => {
+        console.log(error);
+      });
   }
 }
