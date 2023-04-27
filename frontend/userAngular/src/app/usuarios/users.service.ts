d98369d90d9a34bf50c416fe29ea1de1f2b45334
Jacobo-EG <0619975360@uma.es>
2023-04-27 17:15:56 +0200

    Implemented control cookies for jwt, but now login wont display

--- frontend/userAngular/src/app/usuarios/users.service.ts
+++ frontend/userAngular/src/app/usuarios/users.service.ts
@@ -2,14 +2,23 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { usuario } from "../models/usuario.models";
 import { Observable } from "rxjs";
+import { CookieService } from "ngx-cookie-service";
 
 @Injectable({
   providedIn: "root",
 })
 export class usersService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookies: CookieService) {}
 
   login(user: usuario): Observable<any> {
     return this.http.post("http://localhost:8080/usuarios/login", user);
+  }
+
+  setToken(token: string){
+    this.cookies.set("token",token);
+  }
+
+  getToken(){
+    return this.cookies.get("token");
   }
 }
