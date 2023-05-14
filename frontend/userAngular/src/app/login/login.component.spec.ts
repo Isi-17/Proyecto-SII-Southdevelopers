4eb6da00aa1bda31736d5e3f17df4d828d3ba705
Jacobo-EG <0619975360@uma.es>
2023-05-14 21:49:11 +0200

    Began with the tests

--- frontend/userAngular/src/app/login/login.component.spec.ts
+++ frontend/userAngular/src/app/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +9,9 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        HttpClientTestingModule
+      ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -19,5 +23,20 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Debe generar correctamente la pagina de login', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    const emailInput = compiled.querySelector('#email') as HTMLInputElement;
+    expect(emailInput).toBeTruthy();
+    const passwordInput = compiled.querySelector('#password') as HTMLInputElement;
+    expect(passwordInput).toBeTruthy();
+    const loginBoton = compiled.querySelector('#login') as HTMLButtonElement;
+    expect(loginBoton).toBeTruthy();
+    const forgotPasswordBoton = compiled.querySelector('#forgotPassword') as HTMLButtonElement;
+    expect(forgotPasswordBoton).toBeTruthy();
   });
 });
--- frontend/userAngular/src/app/usuarios/users.service.spec.ts
+++ frontend/userAngular/src/app/usuarios/users.service.spec.ts
@@ -1,13 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UsersService } from './users.service';
+import { usersService } from './users.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let service: usersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UsersService);
+    TestBed.configureTestingModule({
+      imports:[
+        HttpClientTestingModule
+      ],
+      declarations:[
+        usersService
+      ]
+    });
+    service = TestBed.inject(usersService);
   });
 
   it('should be created', () => {
