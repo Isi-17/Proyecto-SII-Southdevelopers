5b7b332cad8ec59a46c53e629cd8220e5ac749f0
alvarosh15 <alvaro.s.h.15@gmail.com>
2023-05-18 16:52:02 +0200

    remove studentAngular from base

--- studentAngular/src/app/models/instituto.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Instituto{
-    id: number;
-    nombre: string;
-    direccion1: string;
-    direccion2: string;
-    localidad: string;
-    codigoPostal: number;
-    pais: string;
-}
