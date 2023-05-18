caef0ec9741eb7d03fbfdd9c526052d371452911
Isidro <isidrojova@gmail.com>
2023-05-18 17:30:40 +0200

    Open login when deploy

--- frontend/userAngular/src/app/app.routing.ts
+++ frontend/userAngular/src/app/app.routing.ts
@@ -4,13 +4,19 @@ import { LoginComponent } from "./login/login.component";
 import { ForgotPasswordComponent } from "./forgotPassword/forgotPassword.component";
 
 const appRoutes = [
-  { path: "", component: AppComponent, pathMatch: "full" },
+  { path: "", component: AppComponent, redirectTo: 'login', pathMatch: "full" },
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "forgotPassword", component: ForgotPasswordComponent, pathMatch: "full" },
 ];
 
+// export const routing = RouterModule.forRoot([
+//   { path: "", component: AppComponent, pathMatch: "full" },
+//   { path: "login", component: LoginComponent, pathMatch: "full" },
+//   { path: "forgotPassword", component: ForgotPasswordComponent, pathMatch: "full" },
+// ]);
+
 export const routing = RouterModule.forRoot([
-    { path: "", component: AppComponent, pathMatch: "full" },
+    { path: "", redirectTo: 'login', pathMatch: "full" },
     { path: "login", component: LoginComponent, pathMatch: "full" },
     { path: "forgotPassword", component: ForgotPasswordComponent, pathMatch: "full" },
   ]);
